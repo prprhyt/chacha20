d740f056bbf2591fed7a08537b1631122ad22590
prprhyt <prprhyt@gmail.com>
2019-05-20 23:28:31 +0900

    modify package name

--- chacha/chacha.go
+++ chacha/chacha.go
@@ -4,7 +4,7 @@
 
 // Package chacha implements some low-level functions of the
 // ChaCha cipher family.
-package chacha // import "github.com/aead/chacha20/chacha"
+package chacha
 
 import (
 	"encoding/binary"
